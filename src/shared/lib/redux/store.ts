ed47c920c094add2b20b1f964c6aeb148bd6603c
TolykbaevIlyas <77828905+TolykbaevIlyas@users.noreply.github.com>
2024-01-10 16:29:20 +0600

    Update db form, add api to store

--- src/shared/lib/redux/store.ts
+++ src/shared/lib/redux/store.ts
@@ -1,11 +1,13 @@
-
-
+import { taskApi } from '@/shared/api/TrelloApi';
 import { configureStore } from '@reduxjs/toolkit'
 import { setupListeners } from '@reduxjs/toolkit/query';
 
 export const store = configureStore ({
   reducer:{   
+    [taskApi.reducerPath] : taskApi.reducer,
   },
+  middleware:(getDefaultMiddleware) => 
+  getDefaultMiddleware().concat(taskApi.middleware),
 });
 
 setupListeners(store.dispatch);
