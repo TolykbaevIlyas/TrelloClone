488918f2178b725156382da7b5737f3563bf2dbb
TolykbaevIlyas <77828905+TolykbaevIlyas@users.noreply.github.com>
2024-01-14 22:19:27 +0600

    Add functions to drag n drop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,42 @@ export default function Home() {
     {id: 2, title:"Проверить",items:[{id:4,title:'ГОff'},{id:5,title:'ТУ'},{id:6,title:'Сf'}]},
     {id: 3, title:"Сделано",items:[{id:7,title:'ГОgg'},{id:8,title:'Тда'},{id:9,title:'aС'}]},
   ])
+
+  const [currentBoard, setCurrentBoard] = useState(null)
+  const [currentItem, setCurrentItem] = useState(null)
+
+  function dragOverHandler(e:any){
+    e.preventDefault();
+  }
+  function dragLeaveHandler(e:any){
+    
+  }
+  function dragStartHandler(e:any,board:any,item:any){
+    setCurrentBoard(board);
+    setCurrentItem(item)
+  }
+  function dragEndHandler(e:any){
+    
+  }
+  function drapHandler(e:any,board:any,item:any){
+    e.preventDefault();
+    const currentIndex = currentBoard.items.indexOf(currentItem)
+    currentBoard.items.splice(currentIndex,1)
+    console.log("hello")
+    const dropIndex = board.items.indexOf(item)
+    board.items.splice(dropIndex + 1, 0, currentItem)
+
+    setBoards(boards.map(b => {
+      if(b.id === board.id){
+        return board
+      }
+      if(b.id === currentBoard.id){
+        return currentBoard
+      }
+      return 0
+    }))
+  }
+
   return (
     <main className="flex min-h-screen flex-row items-center justify-between p-24">
       {boards.map(board => 
@@ -16,10 +52,10 @@ export default function Home() {
           <div className='border-b-2'>{board.title}</div>
           {board.items.map(item => 
             <div 
-                onDragOver={(e)=> dragOverHandler(e,board,item)}
+                onDragOver={(e)=> dragOverHandler(e)}
                 onDragLeave={e => dragLeaveHandler(e)}
-                onDragStart={(e)=> dragStartHandler(e)}
-                onDragEnd={(e)=> DragEndHandler(e)}
+                onDragStart={(e)=> dragStartHandler(e,board,item)}
+                onDragEnd={(e)=> dragEndHandler(e)}
                 onDrop={(e)=> drapHandler(e,board,item)}
                 className='cursor-grab m-2 p-2 border-2 rounded-xl' 
                 draggable={true} >
